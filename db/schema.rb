d3233a55f5f3b5ef07e2c113afd11afc8baa1a6a
Francisco <fcalix88@gmail.com>
2013-04-04 00:35:35 -0600

    Cambios a promociones y el signin

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 # from scratch. The latter is a flawed and unsustainable approach (the more migrations
 # you'll amass, the slower it'll run and the greater likelihood for issues).
 #
-# It's strongly recommended to check this file into your version control system
+# It's strongly recommended to check this file into your version control system.
 
 ActiveRecord::Schema.define(:version => 20130327212037) do
 
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(:version => 20130327212037) do
     t.datetime "photo_updated_at"
   end
 
+  create_table "englishes", :force => true do |t|
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
+
   create_table "fotos", :force => true do |t|
     t.datetime "created_at",         :null => false
     t.datetime "updated_at",         :null => false
@@ -30,7 +35,6 @@ ActiveRecord::Schema.define(:version => 20130327212037) do
     t.string   "photo_content_type"
     t.integer  "photo_file_size"
     t.datetime "photo_updated_at"
-
   end
 
   create_table "habitaciones", :force => true do |t|
@@ -42,8 +46,6 @@ ActiveRecord::Schema.define(:version => 20130327212037) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
   end
-
-
 
   create_table "users", :force => true do |t|
     t.string   "email"
